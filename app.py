0c214b8ee463e8dd182778a5235c0773f9681b7b
Utkarsh Chakrwarti <utkarshchakrwarti007@gmail.com>
2023-12-14 04:02:09 +0530

    updated mvp project with better gui

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory, url_for
+from flask import Flask, request, jsonify, send_from_directory, url_for, render_template
 from flask_cors import CORS
 from werkzeug.utils import secure_filename
 import os
@@ -9,88 +9,100 @@ import matplotlib.pyplot as plt
 from colorizers import *
 
 app = Flask(__name__, static_folder='static')
-CORS(app, supports_credentials=True)  # Enable CORS for all routes
-
-# debug mode
-app.debug = False
+CORS(app, supports_credentials=True)
 
 app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png'}
-app.config['MAX_IMAGES'] = 100
+app.config['MAX_IMAGES'] = 10
 
 colorizer_siggraph17 = siggraph17(pretrained=True).eval()
 if torch.cuda.is_available():
     colorizer_siggraph17.cuda()
 
+
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
+
 def process_image(file_path):
+    img = Image.open(file_path)
+
+    # Check if the image is in PNG format
+    if img.format == 'PNG':
+        # Convert PNG to JPEG
+        jpeg_path = os.path.splitext(file_path)[0] + '.jpg'
+        img.convert('RGB').save(jpeg_path)
+        file_path = jpeg_path
+
+    # Continue with the rest of the processing
     img = load_img(file_path)
     (tens_l_orig, tens_l_rs) = preprocess_img(img, HW=(512, 512))
     if torch.cuda.is_available():
         tens_l_rs = tens_l_rs.cuda()
 
-    out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
+    out_img_siggraph17 = postprocess_tens(
+        tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
-    # Save the colorized images
     filename = os.path.splitext(os.path.basename(file_path))[0]
-    #save image with same extension as uploaded image
-    output_path_siggraph17 = os.path.join(app.config['UPLOAD_FOLDER'], f'{filename}_siggraph17{os.path.splitext(file_path)[1]}')
+    output_path_siggraph17 = os.path.join(
+        app.config['UPLOAD_FOLDER'], f'{filename}_colorized{os.path.splitext(file_path)[1]}')
     plt.imsave(output_path_siggraph17, out_img_siggraph17)
 
     return output_path_siggraph17
 
+
 def remove_images():
     while True:
-        time.sleep(420)  # Sleep for 7 minutes
-
-        # Remove images if the number exceeds the limit
-        images = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*.png'))
+        time.sleep(420)
+        images = glob.glob(os.path.join(app.config['UPLOAD_FOLDER'], '*'))
         if len(images) > app.config['MAX_IMAGES']:
-            oldest_images = sorted(images, key=os.path.getctime)[:len(images) - app.config['MAX_IMAGES']]
+            oldest_images = sorted(images, key=os.path.getctime)[
+                :len(images) - app.config['MAX_IMAGES']]
             for image in oldest_images:
                 os.remove(image)
 
+
 def start_remove_images_thread():
-    thread = threading.Timer(420.0, remove_images)
+    thread = threading.Thread(target=remove_images)
     thread.daemon = True
     thread.start()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # Check if the POST request has a file part
-        if 'file' not in request.files:
-            return 'No file selected'
+    start_time = time.time()
+    files = request.files.getlist("file")
+    processed_images = []
 
-        file = request.files['file']
+    for file in files:
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
 
-        # Check if the file is empty
-        if file.filename == '':
-            return 'No file selected'
+            colorized_image_path = process_image(file_path)
+            processed_images.append(
+                url_for('download_file', filename=os.path.basename(colorized_image_path)))
 
-        # Check if the file is allowed
-        if not allowed_file(file.filename):
-            return 'Invalid file format'
+    end_time = time.time()
+    processing_time = end_time - start_time
 
-        # Save the uploaded file
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
+    return jsonify({
+        'processed_images': processed_images,
+        'processing_time': processing_time
+    })
 
-        # Process the uploaded image
-        colorized_image_siggraph17 = process_image(file_path)
 
-        # Return the URL of the colorized image
-        return {'colorized_image_url': url_for('download_file', filename=os.path.basename(colorized_image_siggraph17))}
-
-    return 'Welcome to the Image Colorization API'
-
-@app.route('/uploads/<path:filename>', methods=['GET'])
+@app.route('/uploads/<filename>')
 def download_file(filename):
-    uploads = os.path.join(app.root_path, app.config['UPLOAD_FOLDER'])
-    return send_from_directory(uploads, filename, as_attachment=True)
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 if __name__ == '__main__':
+    start_remove_images_thread()
     app.run()
---
+++ static/js/script.js
@@ -0,0 +1,155 @@
+function showToast() {
+    var toast = document.getElementById("toast");
+    toast.classList.remove("hidden");
+    setTimeout(function(){ toast.classList.add("hidden"); }, 4000);
+}
+
+// Call this function when you want to show the toast
+showToast();
+
+document.addEventListener('DOMContentLoaded', function () {
+    const colorizeButton = document.getElementById('colorizeButton');
+    const dropZone = document.getElementById('dropZone');
+    const imageUploadInput = document.getElementById('imageUpload');
+    const progressBarContainer = document.querySelector('.progress-container');
+    const progressBar = document.getElementById('progressBar');
+    let currentFiles = []; // Array to keep track of current files
+
+    // Dark Mode Toggle Functionality
+    document.getElementById('darkModeSwitch').addEventListener('change', function () {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    //on click of colorize button make progress bar visible
+    colorizeButton.addEventListener('click', function () {
+        progressBarContainer.style.display = 'block';
+    });
+    function syncFilesWithInput() {
+        const dataTransfer = new DataTransfer();
+        currentFiles.forEach(file => dataTransfer.items.add(file));
+        imageUploadInput.files = dataTransfer.files;
+    }
+
+    function removeFileAndThumbnail(index) {
+        currentFiles.splice(index, 1); // Remove file from array
+        syncFilesWithInput(); // Update file input
+        updateThumbnails(); // Update thumbnails
+    }
+
+    function updateThumbnails() {
+        const thumbnailsContainer = document.getElementById('thumbnails');
+        thumbnailsContainer.innerHTML = '';
+        currentFiles.forEach((file, index) => {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                const img = new Image();
+                img.src = e.target.result;
+                img.onload = function () {
+                    const thumbnail = document.createElement('div');
+                    thumbnail.className = 'thumbnail w-24 h-24 relative mr-2';
+                    thumbnail.innerHTML = `<img src="${img.src}" alt="Thumbnail" class="w-full h-full object-cover"><div class="close absolute top-0 right-0 bg-red-500 text-white rounded-full cursor-pointer" onclick="removeFileAndThumbnail(${index})"><i class="fas fa-times"></i></div>`;
+                    // Attach the event listener to the close button
+                    thumbnail.querySelector('.close').addEventListener('click', function () {
+                        removeFileAndThumbnail(index);
+                    });
+
+                    thumbnailsContainer.appendChild(thumbnail);
+                };
+            };
+            reader.readAsDataURL(file);
+        });
+    }
+
+    dropZone.addEventListener('click', function () {
+        if (currentFiles.length === 0) {
+            imageUploadInput.click();
+        }
+    });
+
+    dropZone.addEventListener('dragover', (e) => {
+        e.preventDefault();
+    });
+
+    dropZone.addEventListener('dragenter', (e) => {
+        e.preventDefault();
+    });
+
+    dropZone.addEventListener('dragleave', (e) => {
+        // Optional: handle drag leave event
+    });
+    imageUploadInput.addEventListener('change', function () {
+        currentFiles = Array.from(this.files);
+        updateThumbnails();
+    });
+
+    dropZone.addEventListener('drop', (e) => {
+        e.preventDefault();
+        currentFiles = Array.from(e.dataTransfer.files);
+        syncFilesWithInput();
+        updateThumbnails();
+    });
+
+    document.getElementById('uploadForm').addEventListener('submit', function (event) {
+        event.preventDefault();
+        const imageFiles = imageUploadInput.files;
+        const numImages = imageFiles.length;
+        const totalProcessingTime = 20 * numImages;
+        let progress = 0;
+
+        colorizeButton.disabled = true;
+        progressBarContainer.style.display = 'block';
+        progressBar.style.width = '0%';
+
+        let interval = setInterval(function () {
+            progress += 100 / (totalProcessingTime / 0.5);
+            if (progress >= 85) {
+                clearInterval(interval);
+            }
+            progressBar.style.width = progress + '%';
+        }, 500);
+
+        let formData = new FormData();
+        for (let i = 0; i < imageFiles.length; i++) {
+            formData.append('file', imageFiles[i]);
+        }
+
+        fetch('/upload', {
+            method: 'POST',
+            body: formData,
+        })
+            .then(response => response.json())
+            .then(data => {
+                clearInterval(interval);
+                progressBar.style.width = '100%';
+                setTimeout(() => {
+                    progressBar.style.width = '0%';
+                }, 2000); // Fade out progress bar after 2 seconds
+                progressBarContainer.style.display = 'none';
+                displayProcessedImages(data);
+                colorizeButton.disabled = false;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                alert('An error occurred while processing the images.');
+                clearInterval(interval);
+
+                progressBar.style.width = '0%';
+                progressBarContainer.style.display = 'none';
+                colorizeButton.disabled = false;
+            });
+    });
+
+    function displayProcessedImages(data) {
+        const imageGrid = document.getElementById('imageGrid');
+        const metrics = document.getElementById('metrics');
+        imageGrid.innerHTML = '';
+        metrics.innerHTML = `<p>Processing Time: ${data.processing_time.toFixed(2)} seconds</p>`;
+
+        data.processed_images.forEach(imageUrl => {
+            const imageCard = document.createElement('div');
+            imageCard.className = 'image-card border border-gray-300 p-2 text-center';
+            imageCard.innerHTML = `<img src="${imageUrl}" alt="Colorized Image" class="max-w-full h-auto"><div class="mt-2"><a href="${imageUrl}" download id="downloadButton">Download <i class="fa fa-download"></i></a></div>`;
+            imageGrid.appendChild(imageCard);
+        });
+    }
+});
