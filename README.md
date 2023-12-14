989088cb607978cbf2ac7905dfbc39c60f604f37
Utkarsh Chakrwarti <utkarshchakrwarti007@gmail.com>
2023-12-15 04:35:34 +0530

    fixing thumbnails

--- README.md
+++ README.md
@@ -1,88 +1,116 @@
+# <img src="./static/img/smritink.png" alt="Smritnik" width="50" height="50"> MVP Colorizer (Smritink)
 
-# MVP Colorizer (Smritink)
+
+
+## Application Link: [MVP Colorizer](https://smritink.azurewebsites.net)
 
 ## Introduction
 MVP Colorizer (Smritink) is a Flask-based web application designed for colorizing black and white images using advanced deep learning models. The latest version includes user interface enhancements and improved colorization techniques, making it more efficient and user-friendly.
 
-## Features
-- **Enhanced User Interface**: A more intuitive and responsive design for easy uploading and downloading of images.
-- **Support for Common Image Formats**: Users can upload images in JPG, JPEG, and PNG formats.
-- **Improved Colorization Algorithm**: Utilizes the Siggraph'17 model for more accurate and vibrant colorization.
-- **Interactive Web Interface**: Users can easily navigate through the application to upload and view colorized images.
+## 🚀 Features
+
+| Feature                        | Description                                        |
+| ------------------------------ | -------------------------------------------------- |
+| 🎨 Enhanced User Interface     | Intuitive and responsive design for easy image handling. |
+| 🔄 Support for Common Image Formats | Users can upload images in JPG, JPEG, and PNG formats. |
+| 🌟 Improved Colorization Algorithm | Utilizes the Siggraph'17 model for accurate and vibrant colorization. |
+| 🌐 Interactive Web Interface    | Users can easily navigate through the application for seamless colorization. |
 
 ## Getting Started
+
 ### Requirements
+
 - Python 3.x
 - Flask
 - Additional Python libraries as listed in `requirements.txt`
 
 ### Installation and Setup
+
 1. Clone the repository to your local machine.
-2. Install the required Python libraries using `pip install -r requirements.txt`.
-3. Run the Flask application with `python app.py`.
+    ```bash
+    git clone https://github.com/yourusername/mvp-colorizer.git
+    ```
+
+2. Install the required Python libraries using pip.
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+3. Run the Flask application.
+    ```bash
+    python app.py
+    ```
+
+4. Open your web browser and go to [http://localhost:5000](http://localhost:5000).
 
 ## Usage
+
 ### Uploading Images
-- Navigate to the web interface.
-- Click on the upload section and select a black and white image.
-- Submit the image for colorization.
+
+1. Navigate to the web interface.
+2. Click on the upload section and select a black and white image.
+3. Submit the image for colorization.
 
 ### Downloading Images
+
 - After colorization, the image will be displayed on the web interface.
 - Click the download link to save the colorized image.
-
 
 ## API Reference for MVP Colorizer
 
 The MVP Colorizer Flask application offers a simple API for uploading black and white images and receiving colorized versions in return. Below are the details of the available endpoints.
 
-## Endpoints
+### Endpoints
 
-### 1. Home Page
-- **URL**: `/`
-- **Method**: `GET`
-- **Description**: Serves the home page of the application.
-- **Response**: HTML content of `index.html`.
+1. **Home Page**
 
-### 2. Upload Image
-- **URL**: `/upload`
-- **Method**: `POST`
-- **Description**: Receives one or more black and white images from the user, processes them, and returns paths for the colorized images.
-- **Data Params**: 
-  - `file`: The image file(s) to be uploaded. Must be in `jpg`, `jpeg`, or `png` format.
-- **Success Response**:
-  - **Code**: 200 
-  - **Content**: JSON containing:
-    - `processed_images`: List of URLs for downloading the colorized images.
-    - `processing_time`: Time taken to process the images.
-- **Error Response**:
-  - **Code**: 400 BAD REQUEST
-  - **Content**: Error message.
+    - **URL**: `/`
+    - **Method**: `GET`
+    - **Description**: Serves the home page of the application.
+    - **Response**: HTML content of `index.html`.
 
-### 3. Download Image
-- **URL**: `/uploads/<filename>`
-- **Method**: `GET`
-- **Description**: Allows downloading of a colorized image.
-- **URL Params**:
-  - **Required**: `filename` - Name of the file to be downloaded.
-- **Success Response**:
-  - **Code**: 200
-  - **Content**: The requested image file.
-- **Error Response**:
-  - **Code**: 404 NOT FOUND
-  - **Content**: Error message if the file is not found.
+2. **Upload Image**
+
+    - **URL**: `/upload`
+    - **Method**: `POST`
+    - **Description**: Receives one or more black and white images from the user, processes them, and returns paths for the colorized images.
+    - **Data Params**: `file` - The image file(s) to be uploaded. Must be in `jpg`, `jpeg`, or `png` format.
+    - **Success Response**: 
+        - **Code**: 200 
+        - **Content**: JSON containing:
+            - `processed_images`: List of URLs for downloading the colorized images.
+            - `processing_time`: Time taken to process the images.
+    - **Error Response**: 
+        - **Code**: 400 BAD REQUEST
+        - **Content**: Error message.
+
+3. **Download Image**
+
+    - **URL**: `/uploads/<filename>`
+    - **Method**: `GET`
+    - **Description**: Allows downloading of a colorized image.
+    - **URL Params**: `filename` (Required) - Name of the file to be downloaded.
+    - **Success Response**: 
+        - **Code**: 200
+        - **Content**: The requested image file.
+    - **Error Response**: 
+        - **Code**: 404 NOT FOUND
+        - **Content**: Error message if the file is not found.
 
 ## Additional Information
+
 - The application uses the `colorizers` package to process the images.
 - Images are temporarily stored in an `uploads` folder and are automatically deleted after a certain threshold to manage storage.
 - The server starts a background thread (`start_remove_images_thread`) to remove old images from the server.
 
+## 🤝 Contributing
 
-## Contributing
 Contributions to MVP Colorizer are welcome. Please feel free to suggest features, report bugs, or submit pull requests via GitHub.
 
-## License
+## 📄 License
+
 This project is released under the [MIT License](LICENSE.md).
 
-## Acknowledgements
+## 🙏 Acknowledgements
+
 Special thanks to the contributors and the open-source community for the support and resources that made this project possible.
--- static/js/script.js
+++ static/js/script.js
@@ -3,16 +3,24 @@ const dropZone = document.getElementById('dropZone');
 const imageUploadInput = document.getElementById('imageUpload');
 const progressBarContainer = document.querySelector('.progress-container');
 const progressBar = document.getElementById('progressBar');
+const thumbnailsContainer = document.getElementById('thumbnails');
+const darkModeSwitch = document.getElementById('darkModeSwitch');
+const uploadForm = document.getElementById('uploadForm');
+const colorizerModelSelect = document.getElementById('colorizerModel');
+
 let currentFiles = []; // Array to keep track of current files
 
 function showToast() {
-    var toast = document.getElementById("toast");
-    toast.classList.remove("hidden");
-    setTimeout(function () { toast.classList.add("hidden"); }, 4000);
+    const toast = document.getElementById('toast');
+    toast.classList.remove('hidden');
+    setTimeout(() => toast.classList.add('hidden'), 4000);
 }
 
-// Call this function when you want to show the toast
-setTimeout(showToast, 1000);
+function changeThemeColorBasedOnSystemPreference() {
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+    darkModeSwitch.checked = prefersDarkScheme.matches;
+    document.body.classList.toggle('dark-mode', prefersDarkScheme.matches);
+}
 
 function syncFilesWithInput() {
     const dataTransfer = new DataTransfer();
@@ -21,46 +29,125 @@ function syncFilesWithInput() {
 }
 
 function removeFileAndThumbnail(index) {
-    currentFiles.splice(index, 1); // Remove file from array
-    syncFilesWithInput(); // Update file input
-    updateThumbnails(); // Update thumbnails
+    currentFiles.splice(index, 1);
+    syncFilesWithInput();
+    updateThumbnails();
+}
+
+function createThumbnail(file, index) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+        const img = new Image();
+        img.src = e.target.result;
+        img.onload = function () {
+            const thumbnail = document.createElement('div');
+            thumbnail.className = 'thumbnail w-24 h-24 relative mr-2';
+            thumbnail.innerHTML = `<img src="${img.src}" alt="Thumbnail" class="w-full h-full object-cover"><div class="close absolute top-0 right-0 bg-red-500 text-white rounded-full cursor-pointer" data-index="${index}"><i class="fas fa-times"></i></div>`;
+            thumbnailsContainer.appendChild(thumbnail);
+        };
+    };
+    reader.readAsDataURL(file);
 }
 
 function updateThumbnails() {
-    const thumbnailsContainer = document.getElementById('thumbnails');
     thumbnailsContainer.innerHTML = '';
     currentFiles.forEach((file, index) => {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-            const img = new Image();
-            img.src = e.target.result;
-            img.onload = function () {
-                const thumbnail = document.createElement('div');
-                thumbnail.className = 'thumbnail w-24 h-24 relative mr-2';
-                thumbnail.innerHTML = `<img src="${img.src}" alt="Thumbnail" class="w-full h-full object-cover"><div class="close absolute top-0 right-0 bg-red-500 text-white rounded-full cursor-pointer" onclick="removeFileAndThumbnail(${index})"><i class="fas fa-times"></i></div>`;
-                // Attach the event listener to the close button
-                thumbnail.querySelector('.close').addEventListener('click', function () {
-                    removeFileAndThumbnail(index);
-                });
+        createThumbnail(file, index);
+    });
+}
 
-                thumbnailsContainer.appendChild(thumbnail);
-            };
-        };
-        reader.readAsDataURL(file);
+function handleFileInput() {
+    currentFiles = Array.from(this.files);
+    updateThumbnails();
+}
+
+function handleDrop(e) {
+    e.preventDefault();
+    currentFiles = Array.from(e.dataTransfer.files);
+    syncFilesWithInput();
+    updateThumbnails();
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault();
+
+    const colorizerModel = colorizerModelSelect.options[colorizerModelSelect.selectedIndex].value;
+    const imageFiles = imageUploadInput.files;
+    const numImages = imageFiles.length;
+    const totalProcessingTime = 20 * numImages;
+    let progress = 0;
+
+    colorizeButton.disabled = true;
+    progressBarContainer.style.display = 'block';
+    progressBar.style.width = '0%';
+
+    let interval = setInterval(function () {
+        progress += 100 / (totalProcessingTime / 0.5);
+        if (progress >= 85) {
+            clearInterval(interval);
+        }
+        progressBar.style.width = progress + '%';
+    }, 500);
+
+    let formData = new FormData();
+    formData.append('colorizer_model', colorizerModel);
+
+    for (let i = 0; i < imageFiles.length; i++) {
+        formData.append('file', imageFiles[i]);
+    }
+
+    fetch('/upload', {
+        method: 'POST',
+        body: formData,
+    })
+        .then(response => response.json())
+        .then(data => {
+            clearInterval(interval);
+            progressBar.style.width = '100%';
+            setTimeout(() => {
+                progressBar.style.width = '0%';
+            }, 2000);
+            progressBarContainer.style.display = 'none';
+            displayProcessedImages(data);
+            colorizeButton.disabled = false;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('An error occurred while processing the images.');
+            clearInterval(interval);
+
+            progressBar.style.width = '0%';
+            progressBarContainer.style.display = 'none';
+            colorizeButton.disabled = false;
+        });
+}
+
+function displayProcessedImages(data) {
+    const imageGrid = document.getElementById('imageGrid');
+    const metrics = document.getElementById('metrics');
+    imageGrid.innerHTML = '';
+    metrics.innerHTML = `<p>Processing Time: ${data.processing_time.toFixed(2)} seconds</p><br>`;
+
+    data.processed_images.forEach(imageUrl => {
+        const imageCard = document.createElement('div');
+        imageCard.className = 'image-card border border-gray-300 p-2 text-center';
+        imageCard.innerHTML = `<img src="${imageUrl}" alt="Colorized Image" class="max-w-full h-auto"><div class="mt-2">
+        <a href="${imageUrl}" download id="downloadButton"> Download <i class="fa fa-download"></i></a></div>`;
+        imageGrid.appendChild(imageCard);
     });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    // Dark Mode Toggle Functionality
-    document.getElementById('darkModeSwitch').addEventListener('change', function () {
+    changeThemeColorBasedOnSystemPreference();
+    setTimeout(showToast, 1000);
+
+    darkModeSwitch.addEventListener('change', function () {
         document.body.classList.toggle('dark-mode');
     });
 
-    //on click of colorize button make progress bar visible
     colorizeButton.addEventListener('click', function () {
         progressBarContainer.style.display = 'block';
     });
-
 
     dropZone.addEventListener('click', function () {
         if (currentFiles.length === 0) {
@@ -68,97 +155,20 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    dropZone.addEventListener('dragover', (e) => {
-        e.preventDefault();
-    });
+    dropZone.addEventListener('dragover', e => e.preventDefault());
+    dropZone.addEventListener('dragenter', () => { /* Optional: handle drag enter event */ });
+    dropZone.addEventListener('dragleave', () => { /* Optional: handle drag leave event */ });
 
-    dropZone.addEventListener('dragenter', (e) => {
-        e.preventDefault();
-    });
+    imageUploadInput.addEventListener('change', handleFileInput);
+    dropZone.addEventListener('drop', handleDrop);
 
-    dropZone.addEventListener('dragleave', (e) => {
-        // Optional: handle drag leave event
-    });
-    imageUploadInput.addEventListener('change', function () {
-        currentFiles = Array.from(this.files);
-        updateThumbnails();
-    });
+    uploadForm.addEventListener('submit', handleFormSubmit);
+});
 
-    dropZone.addEventListener('drop', (e) => {
-        e.preventDefault();
-        currentFiles = Array.from(e.dataTransfer.files);
-        syncFilesWithInput();
-        updateThumbnails();
-    });
-
-    document.getElementById('uploadForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-    
-        // Get the selected colorizer model
-        const colorizerModelSelect = document.getElementById('colorizerModel');
-        const colorizerModel = colorizerModelSelect.options[colorizerModelSelect.selectedIndex].value;
-    
-        const imageFiles = imageUploadInput.files;
-        const numImages = imageFiles.length;
-        const totalProcessingTime = 20 * numImages;
-        let progress = 0;
-
-        colorizeButton.disabled = true;
-        progressBarContainer.style.display = 'block';
-        progressBar.style.width = '0%';
-
-        let interval = setInterval(function () {
-            progress += 100 / (totalProcessingTime / 0.5);
-            if (progress >= 85) {
-                clearInterval(interval);
-            }
-            progressBar.style.width = progress + '%';
-        }, 500);
-
-        let formData = new FormData();
-        formData.append('colorizer_model', colorizerModel);  // Pass the selected colorizer model
-    
-        for (let i = 0; i < imageFiles.length; i++) {
-            formData.append('file', imageFiles[i]);
-        }
-    
-        fetch('/upload', {
-            method: 'POST',
-            body: formData,
-        })
-        .then(response => response.json())
-        .then(data => {
-                clearInterval(interval);
-                progressBar.style.width = '100%';
-                setTimeout(() => {
-                    progressBar.style.width = '0%';
-                }, 2000); // Fade out progress bar after 2 seconds
-                progressBarContainer.style.display = 'none';
-                displayProcessedImages(data);
-                colorizeButton.disabled = false;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('An error occurred while processing the images.');
-                clearInterval(interval);
-
-                progressBar.style.width = '0%';
-                progressBarContainer.style.display = 'none';
-                colorizeButton.disabled = false;
-            });
-    });
-
-    function displayProcessedImages(data) {
-        const imageGrid = document.getElementById('imageGrid');
-        const metrics = document.getElementById('metrics');
-        imageGrid.innerHTML = '';
-        metrics.innerHTML = `<p>Processing Time: ${data.processing_time.toFixed(2)} seconds</p>`;
-
-        data.processed_images.forEach(imageUrl => {
-            const imageCard = document.createElement('div');
-            imageCard.className = 'image-card border border-gray-300 p-2 text-center';
-            imageCard.innerHTML = `<img src="${imageUrl}" alt="Colorized Image" class="max-w-full h-auto"><div class="mt-2"><a href="${imageUrl}" download id="downloadButton">Download <i class="fa fa-download"></i></a></div>`;
-            imageGrid.appendChild(imageCard);
-        });
+thumbnailsContainer.addEventListener('click', function (e) {
+    const closeButton = e.target.closest('.close');
+    if (closeButton) {
+        const index = closeButton.getAttribute('data-index');
+        removeFileAndThumbnail(index);
     }
 });
